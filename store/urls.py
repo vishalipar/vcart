caff7dff3e9c2c2fa03f5bff79421604de50869d
vishalipar <vishalipar04@gmail.com>
2024-11-06 15:19:47 +0530

    created store page

---
+++ store/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path 
+from . import views
+
+urlpatterns = [
+    path('', views.store, name='store'),
+    path('category/', views.prod_detail, name='prod_detail'),
+    
+]
